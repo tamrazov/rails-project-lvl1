9efa2b72d46e934934006fa669acf0973e2e1749
Alex <box-570@yandex.ru>
2021-07-10 22:29:37 +0300

    add class Tag and method build

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -5,4 +5,19 @@ require_relative "hexlet_code/version"
 module HexletCode
   class Error < StandardError; end
   # Your code goes here...
+  class Tag
+    def self.build(tag, *attrs)
+      result = "<#{tag}"
+      attrs.each do |items|
+        items.each do |key, value|
+          result = "#{result} #{key}=\"#{value}\""
+        end
+      end
+      if block_given?
+        result = "#{result}>#{yield}</#{tag}>"
+      else
+        result = "#{result}>"
+      end
+    end
+  end
 end
